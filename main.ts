2c8b68060ceaf036d16ff6af9566427c65ef7f13
mathias-coronel <154088930+mathias-coronel@users.noreply.github.com>
2023-12-19 13:20:55 -0500

    finished level 3

--- main.ts
+++ main.ts
@@ -28,8 +28,8 @@ let fish_left_per_level: number[] = []
 let array_index = 0
 let fish: Sprite = null
 fish_left_per_level = [
-2,
-2,
+10,
+12,
 14,
 16,
 20
@@ -49,8 +49,9 @@ false,
 false
 ]
 let active_fish = fishp_index
-let asset_imgs = [assets.image`fish1`, assets.image`fisho`]
-let asset_anims = [assets.animation`fishp`, assets.animation`fisho`]
+let asset_imgs = [assets.image`fish1`, assets.image`fisho`, assets.image`crab`]
+let asset_anims = [assets.animation`fishp`, assets.animation`fisho`, assets.animation`crab`]
+let fish_speed = [-20, -30, -40, -50, -60]
 game.onUpdateInterval(1000, function () {
     if (fish_left_per_level[active_fish] > 0) {
         fish = sprites.create(asset_imgs[active_fish], SpriteKind.Enemy)
@@ -61,7 +62,7 @@ game.onUpdateInterval(1000, function () {
         true
         )
         fish.setPosition(164, randint(0, 110))
-        fish.setVelocity(-30, 0)
+        fish.setVelocity(fish_speed[active_fish], 0)
         fish.data.num_hits = 0
 fish.data.max_num_hits = max_hits[active_fish]
 fish.data.di = active_fish
