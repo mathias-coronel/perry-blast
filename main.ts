96051a8dd000638c817d31ee4c082b5caeb961a0
mathias-coronel <154088930+mathias-coronel@users.noreply.github.com>
2023-12-19 10:54:17 -0500

    i have a bug left

--- main.ts
+++ main.ts
@@ -8,23 +8,27 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     cannonball.x = sub.x + 5
     cannonball.setScale(1, ScaleAnchor.Middle)
 })
-// TODO: fishes are not dying - figure out why and fix it
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (cannonball, fishp) {
     sprites.destroy(cannonball)
     fishp.data.num_hits = fishp.data.num_hits + 1
 if (fishp.data.num_hits > fishp.data.max_num_hits) {
         sprites.destroy(fishp)
-        num_fishp_left = num_fishp_left - 1
+        array_index = parseInt(fishp.data.di)
+        fish_left_per_level[array_index] = fish_left_per_level[array_index] - 1
+        game.showLongText(fish_left_per_level[array_index], DialogLayout.Center)
     }
 })
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
     game.setGameOverMessage(false, "fail :(")
     game.gameOver(false)
 })
+let array_index = 0
 let cannonball: Sprite = null
-let num_fishp_left = 0
 let sub: Sprite = null
+let fish_left_per_level: number[] = []
+let fishp_index = 0
 let fish: Sprite = null
+fish_left_per_level = [10]
 game.showLongText("Hit the fish with cannonballs and don't let them touch you! Good luck", DialogLayout.Center)
 sub = sprites.create(assets.image`subby`, SpriteKind.Player)
 sub.setPosition(9, 66)
@@ -32,9 +36,15 @@ controller.moveSprite(sub, 100, 100)
 sub.setScale(2, ScaleAnchor.Middle)
 sub.setStayInScreen(true)
 let max_hits_fishp = 3
-num_fishp_left = 10
+let level_complete = [
+false,
+false,
+false,
+false,
+false
+]
 game.onUpdateInterval(1000, function () {
-    if (num_fishp_left > 0) {
+    if (fish_left_per_level[fishp_index] > 0) {
         fish = sprites.create(assets.image`fish1`, SpriteKind.Enemy)
         animation.runImageAnimation(
         fish,
@@ -46,8 +56,12 @@ game.onUpdateInterval(1000, function () {
         fish.setVelocity(-30, 0)
         fish.data.num_hits = 0
 fish.data.max_num_hits = max_hits_fishp
+fish.data.di = fishp_index
     } else {
-        game.showLongText("Level 1 complete!", DialogLayout.Center)
+        if (level_complete[fishp_index] == false) {
+            game.showLongText("Level 1 complete!", DialogLayout.Center)
+            level_complete[fishp_index] = true
+        }
     }
 })
 game.onUpdateInterval(500, function () {
